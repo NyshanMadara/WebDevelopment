e59e4c67ce1dcb7ed56690c3de6c42ded24675fa
NyshanMadara <47220723+NyshanMadara@users.noreply.github.com>
2020-02-12 05:12:40 +0600

    Add files via upload

---
+++ Lab3-Angular/src/app/product-item/product-item.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { items } from '../items';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent {
+  items = items;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
